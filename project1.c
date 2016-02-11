4914cf7c114cc7381a944a711d2bfc5943e255cd
tarquink <tarquink@mail.gvsu.edu>
2016-02-11 16:05:49 -0500

    added solution file

---
+++ project1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void readFreq (float given[], char fname[]);
+void calcFreq (float found[], char fname[]);
+char rotate (char ch, int num);
+int findKet (float given[], float found[]);
+void decrypt (int key，  char fname[]);
