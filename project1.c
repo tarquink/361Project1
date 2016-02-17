e0d7603fcf66cd3c4a5bfff746dd76cd9b285866
tarquink <tarquink@mail.gvsu.edu>
2016-02-17 02:44:41 -0500

    completed calcFreq

--- project1.c
+++ project1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void readFreq (float[], char[]);
 void calcFreq (float[], char[]);
@@ -11,9 +12,11 @@ void decrypt (int, char[]);
 int main(){
   float freq[26];
   readFreq(freq, "/home/kevin/Documents/361/361Project1/LetFreq.txt");
+  
   return 0;
 }
 
+// Load array given with the letter frequencies for English from file LetFreq.txt
 void readFreq(float given[], char fname[]){
   FILE *file;
   
@@ -34,10 +37,15 @@ void readFreq(float given[], char fname[]){
   fclose(file);
 }
 
+// Read the encoded text from an input file and accumulate the letter frequency                  
+// data for the encoded text. Store the frequency data in array found.
 void calcFreq(float found[], char fname[]){
  FILE *file;
- char message[200];
+ char message[100];
  int i = 0;
+ float messageFreq[26];
+ float total = 0.0;
+ 
  file = fopen(fname, "r");
  
  if(file == NULL){
@@ -45,7 +53,25 @@ void calcFreq(float found[], char fname[]){
     exit(0);
  }
   
-  while(fscanf(file, " %c", &found[i])!=EOF){
-   i++
+  while(fscanf(file, " %c", &message[i])!=EOF){
+   i++;
   }
+  
+  i = 0;
+  while(message[i] != '\0'){
+    if(tolower(message[i]) >= 'a' && tolower(message[i]) <= 'z'){
+      messageFreq[message[i] - 'a']++;
+    }
+    i++;
+  }
+  
+  for(i=0; i < 26; i++){
+    total = total + messageFreq[i];
+  }
+  
+  for(i=0; i < 26; i++){
+    messageFreq[i] = messageFreq[i]/total;
+  }
+  
+  fclose(file);
 }
