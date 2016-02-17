a9b1384377dae7b1f798c9e6930a8b3792fc2c6a
tarquink <tarquink@mail.gvsu.edu>
2016-02-17 01:29:30 -0500

    completed read freq

--- project1.c
+++ project1.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void readFreq (float given[], char fname[]);
-void calcFreq (float found[], char fname[]);
-char rotate (char ch, int num);
-int findKet (float given[], float found[]);
-void decrypt (int key，  char fname[]);
+void readFreq (float[], char[]);
+void calcFreq (float[], char[]);
+char rotate (char, int);
+int findKet (float[], float[]);
+void decrypt (int, char[]);
+
+
+int main(){
+  float freq[26];
+  readFreq(freq, "/home/kevin/Documents/361/361Project1/LetFreq.txt");
+  return 0;
+}
+
+void readFreq(float given[], char fname[]){
+  FILE *file;
+  
+  char ch;
+  int i = 0;
+  
+  file = fopen(fname, "r");
+  
+  if(file == NULL){
+    printf("FILE READ ERROR\n");
+    exit(0);
+  }
+  
+  for(i=0; i<26; i++){
+    fscanf(file," %c%f", &ch, &given[i]);
+  }
+  
+  fclose(file);
+}
+
+void calcFreq(float found[], char fname[]){
+ FILE *file;
+ char message[200];
+ int i = 0;
+ file = fopen(fname, "r");
+ 
+ if(file == NULL){
+    printf("FILE READ ERROR\n");
+    exit(0);
+ }
+  
+  while(fscanf(file, " %c", &found[i])!=EOF){
+   i++
+  }
+}
